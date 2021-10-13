4a4c937378c068b42cfd5c3ffc2e436a3064d359
DELL <DELL@DESKTOP-EAAVRMF>
2021-10-13 10:27:00 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp4
+class NombrePremier
+{
+
+    static void Main()
+    {
+        int[] diviseurs = { 2, 3, 5, 7 };
+        int i = 10, j;
+
+
+        while (i <= 2000)
+        {
+
+            for (j = 0; j <= 3; j++)
+            {
+                if (i % diviseurs[j] == 0) i++;
+                else if (j == 3)
+                {
+                    Console.WriteLine(i + " est premier");
+                    i++;
+                }
+            }
+        }
+    }
+}
